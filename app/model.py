6033ac1ef58134f828cf4abcf973f2440a1e9b56
Александр Чаусов <traktoro_13@mail.ru>
2016-03-07 21:30:55 +0300

    в БД добавлена сущность для ролей

--- app/model.py
+++ app/model.py
@@ -1,9 +1,42 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask.ext.login import UserMixin
+from flask.ext.login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app, request
 from datetime import datetime
+
+class Permission:
+    CHECKIN = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATOR= 0x08
+    ADMINISTER = 0x80
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.CHECKIN | Permission.COMMENT | Permission.WRITE_ARTICLES),
+            'LockedUser': (Permission.CHECKIN),
+            'Moderator': (Permission.CHECKIN | Permission.COMMENT | Permission.WRITE_ARTICLES | Permission.MODERATOR),
+            'Administrator': (0xff)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name = r).first()
+            if role is None:
+                role = Role(name = r)
+            role.permissions = roles[r][0]
+            db.session.add(role)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -13,12 +46,21 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default = False)
     nfc_label = db.Column(db.String(64), unique = True, index = True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     #other info
     first_name = db.Column(db.String(64))
     middle_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
     member_since = db.Column(db.DateTime(), default = datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default = datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['TIMELOCK_ADMIN']:
+                self.role = Role.query.filter_by(permissions = 0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(permissions = 0x07).first()
 
     @property
     def password(self):
@@ -50,9 +92,27 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def can(self, permissions):
+        return self.role is not None and (self.role.permissions & permissions) == permissions
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
 
 @login_manager.user_loader
 def load_user(user_id):
--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[TimeLock]'
-    FLASKY_MAIL_SENDER = 'TimeLock Admin <chausov@surfstudio.ru>'
-    FLASKY_ADMIN = os.environ.get('TIMELOCK_ADMIN')
+    TIMELOCK_MAIL_SUBJECT_PREFIX = '[TimeLock]'
+    TIMELOCK_MAIL_SENDER = 'TimeLock Admin <chausov@surfstudio.ru>'
+    TIMELOCK_ADMIN = os.environ.get('TIMELOCK_ADMIN')
 
     @staticmethod
     def init_app(app):
