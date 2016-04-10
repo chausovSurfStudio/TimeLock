84ec00734afd15496b1a5bef7d523979ca5ed440
Александр Чаусов <traktoro_13@mail.ru>
2016-04-10 23:35:22 +0300

    залогиниться можно только после подтверждения аккаунта

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,6 +15,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
+        if not user.confirmed:
+            flash('First, you have to verify your account. Click on the link in your mail.')
+            return redirect(url_for('main.index'))
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
