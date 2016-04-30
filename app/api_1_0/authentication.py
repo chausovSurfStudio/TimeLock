316c899321cb6644bcd00652a4de4225c748fe64
Александр Чаусов <traktoro_13@mail.ru>
2016-04-30 18:31:45 +0300

    исправлена ошибка в сценарии аутентификации

--- app/api_1_0/authentication.py
+++ app/api_1_0/authentication.py
@@ -15,7 +15,7 @@ def verify_password(email_or_token, password):
         g.token_used = True
         return g.current_user is not None
     user = User.query.filter_by(email = email_or_token).first()
-    if not User:
+    if user is None:
         return False
     g.current_user = user
     g.token_used = False
