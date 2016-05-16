c3c0d2bac5c2b4d64ace1dfea959ced0e7241cf8
Александр Чаусов <traktoro_13@mail.ru>
2016-05-17 01:24:20 +0300

    реализованы и протестированы методы API на удаление, изменение и создание чекинов

--- app/api_1_0/checkins.py
+++ app/api_1_0/checkins.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from flask import jsonify, request, g, abort, url_for, current_app
 from app import db
 from app.model import Checkin, User
 from . import api
-from .errors import forbidden
+from .errors import forbidden, bad_request
+from datetime import datetime
+import re
 
 @api.route('/checkins/')
 def get_checkins_page():
@@ -25,3 +29,66 @@ def get_checkins_page():
 	result_json["times"].append(cache)
 	result_json["monday"] = keys[0].strftime('%d.%m.%Y %H:%M:%S')
 	return jsonify(result_json)
+
+@api.route('/checkins/<int:checkin_id>', methods = ['DELETE'])
+def delete_checkin(checkin_id):
+	checkin = Checkin.query.filter_by(id = checkin_id).first()
+	if not checkin:
+		return bad_request(u'Запрошенный к удалению чекин не найден')
+	if checkin.user_id != g.current_user.id:
+		return bad_request(u'Вы не можете изменять чекины других пользователей')
+	db.session.delete(checkin)
+	return jsonify({'success': True})
+
+@api.route('/checkins/', methods = ['POST'])
+def create_checkin():
+	json = request.json
+	date_string = json.get('date_string', "")
+	if date_string == '':
+		return bad_request(u'Неверные параметры запроса')
+	regular_expression = re.compile("\d{1,2}\.\d{1,2}\.\d{4} \d{1,2}:\d{2}:\d{2}")
+	if not regular_expression.match(date_string):
+		return bad_request(u'Неверные параметры запроса')
+	date = datetime.strptime(date_string, "%d.%m.%Y %H:%M:%S")
+	if date > datetime.now():
+		return bad_request(u'Вы не можете создать чекин заранее, выберите более раннюю дату')
+	checkin = Checkin(user_id = g.current_user.id, time = date, trustLevel = False)
+	db.session.add(checkin)
+	db.session.commit()
+	result_json = {}
+	result_json['checkin'] = checkin.to_json(checkin.time.date().strftime('%d.%m.%Y %H:%M:%S'))
+	result_json['success'] = True
+	return jsonify(result_json)
+
+@api.route('/checkins/<int:checkin_id>', methods = ['PUT'])
+def edit_checkin(checkin_id):
+	checkin = Checkin.query.filter_by(id = checkin_id).first()
+	if not checkin:
+		return bad_request(u'Запрошенный чекин не найден, изменение данных невозможно')
+	if checkin.user_id != g.current_user.id:
+		return bad_request(u'Вы не можете изменять чекины других пользователей')
+	json = request.json
+	date_string = json.get('date_string', "")
+	if date_string == '':
+		return bad_request(u'Неверные параметры запроса')
+	regular_expression = re.compile("\d{1,2}\.\d{1,2}\.\d{4} \d{1,2}:\d{2}:\d{2}")
+	if not regular_expression.match(date_string):
+		return bad_request(u'Неверные параметры запроса')
+	date = datetime.strptime(date_string, "%d.%m.%Y %H:%M:%S")
+	if date > datetime.now():
+		return bad_request(u'Вы не можете создать чекин заранее, выберите более раннюю дату')
+	checkin.time = date
+	db.session.add(checkin)
+	db.session.commit()
+	result_json = {}
+	result_json['checkin'] = checkin.to_json(checkin.time.date().strftime('%d.%m.%Y %H:%M:%S'))
+	result_json['success'] = True
+	return jsonify(result_json)
+
+
+
+
+
+
+
+
